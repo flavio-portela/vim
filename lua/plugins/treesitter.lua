8ad045865523671c84ce1c980cb52f36d422e5b8
Flavio Portela <portela828@gmail.com>
2024-01-01 20:49:56 -0800

    basic setup

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,14 @@
+
+  return {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      local treesitter = require("nvim-treesitter.configs")
+      treesitter.setup({
+        ensure_installed = {"lua", "vim", "javascript", "python", "typescript"},
+        sync_install = false,
+        highlight = {enable = true},
+        indent = {enable = true},
+      })
+    end
+  }
